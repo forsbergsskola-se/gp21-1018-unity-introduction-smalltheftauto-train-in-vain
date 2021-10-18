878226bf74815d6ad0d8e9337664e7bfed406ad7
InaSLew <ina.shenlewander@gmail.com>
2021-10-18 17:13:06 +0200

    Added delayed camera-following-player

--- projects/SmallTheftAuto/Assets/Scripts/FollowCamera.cs
+++ projects/SmallTheftAuto/Assets/Scripts/FollowCamera.cs
@@ -4,42 +4,16 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    private const float CameraHeightOffset = -10;
     public GameObject target;
     
-    
-    // Update is called once per frame
     void Update()
     {
-        // Get the camera coordinates.
         var cameraCoordinates = transform.position;
-        
-        // Get the players coordinates
         var targetCoordinates = target.transform.position;
-        //
-        // transform.position = Mathf.Lerp(cameraCoordinates, targetCoordinates, 1, 1);
+        var newCameraCoordinates = Vector3.Lerp(cameraCoordinates, targetCoordinates, 0.1f);
+        newCameraCoordinates.z = CameraHeightOffset;
 
-        Vector3 temp;
-        temp.z = -10;
-        temp.y = Mathf.Lerp(cameraCoordinates.y, targetCoordinates.y, 0.1f);
-        temp.x = Mathf.Lerp(cameraCoordinates.x, targetCoordinates.x, 0.1f);
-
-        Debug.Log(temp);
-        Debug.Log("Target:");
-        Debug.Log(targetCoordinates);
-        
-        transform.position = temp;
-        
-        // Set them as a target?
-
-        // Slowly move the camera in that direction?
-
-        // Accerlerate in that direction?
+        transform.position = newCameraCoordinates;
     }
 }
