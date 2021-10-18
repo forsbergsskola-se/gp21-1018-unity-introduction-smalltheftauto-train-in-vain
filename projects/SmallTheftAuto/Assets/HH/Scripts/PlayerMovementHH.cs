7cb1a63d842378db2180fff5a7838d52b08e544e
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-10-18 16:39:19 +0200

    rotation added

--- projects/SmallTheftAuto/Assets/HH/Scripts/PlayerMovementHH.cs
+++ projects/SmallTheftAuto/Assets/HH/Scripts/PlayerMovementHH.cs
@@ -6,49 +6,49 @@ public class PlayerMovementHH : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 20f;
+    public float rotationSpeed = 60f;
     public Rigidbody2D rb;
-    void Start()
-    {
-        rb = this.GetComponent<Rigidbody2D>();
-    }
 
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal") * speed;
-        var vertical = Input.GetAxis("Vertical") * speed;
-        rb.velocity = new Vector2(horizontal, vertical);
+        var horizontal = Input.GetAxis("Horizontal") * rotationSpeed* Time.deltaTime;
+        var vertical = Input.GetAxis("Vertical") * speed* Time.deltaTime;
+        transform.Translate(0, vertical, 0);
+        transform.Rotate(0, 0, -horizontal);
+        
+
+
+        // Update is called once per frame
+        /*void Update()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(0f, 0.05f, 0f);
+                transform.Rotate(0,0,0);
+            }
+            
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(0f, -0.05f, 0f);
+                transform.Rotate(0,0,180);
+    
+            }
+            
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-0.05f, 0f, 0f);
+                transform.Rotate(0,0,90);
+            }
+            
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(0.05f, 0f, 0f);
+                transform.Rotate(0,0,270);
+    
+            }
+    
+    
+            
+        }*/
     }
-
-    // Update is called once per frame
-    /*void Update()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(0f, 0.05f, 0f);
-            transform.Rotate(0,0,0);
-        }
-        
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(0f, -0.05f, 0f);
-            transform.Rotate(0,0,180);
-
-        }
-        
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-0.05f, 0f, 0f);
-            transform.Rotate(0,0,90);
-        }
-        
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(0.05f, 0f, 0f);
-            transform.Rotate(0,0,270);
-
-        }
-
-
-        
-    }*/
 }
