58914bdf0bc0b907e12080c2660c67863b74b27c
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-10-19 23:46:43 +0200

    Implemented Sprite Changer

--- projects/SmallTheftAuto/Assets/HH/Scripts/SpriteChangerHH.cs
+++ projects/SmallTheftAuto/Assets/HH/Scripts/SpriteChangerHH.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,49 @@ using UnityEngine;
 public class SpriteChangerHH : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
-    void Start()
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public Sprite sprite3;
+    public Sprite sprite4;
+    public Sprite sprite5;
+    public Sprite sprite6;
+  
+    
+    private void Start()
     {
-        
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (Input.GetKeyDown("1"))
+        {
+            spriteRenderer.sprite = sprite1;
+        }
         
+        if (Input.GetKeyDown("2"))
+        {
+            spriteRenderer.sprite = sprite2;
+        }
+        
+        if (Input.GetKeyDown("3"))
+        {
+            spriteRenderer.sprite = sprite3;
+        }
+        
+        if (Input.GetKeyDown("4"))
+        {
+            spriteRenderer.sprite = sprite4;
+        }
+        
+        if (Input.GetKeyDown("5"))
+        {
+            spriteRenderer.sprite = sprite5;
+        }
+        
+        if (Input.GetKeyDown("6"))
+        {
+            spriteRenderer.sprite = sprite6;
+        }
     }
 }
