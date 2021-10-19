185a53cc576d490244f02f521100897722446624
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-10-19 13:18:33 +0200

    Update PlayerMovementHH.cs

--- projects/SmallTheftAuto/Assets/HH/Scripts/PlayerMovementHH.cs
+++ projects/SmallTheftAuto/Assets/HH/Scripts/PlayerMovementHH.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class NewBehaviourScriptHH : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
