4a5de3c8fbe00a0181726ec2d8c5620577d4ba42
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-10-20 00:14:14 +0200

    Added Sprite Changer to Main
    Use 1-6 to toggle different skins

--- projects/SmallTheftAuto/Assets/Scripts/PlayerSpriteChanger.cs
+++ projects/SmallTheftAuto/Assets/Scripts/PlayerSpriteChanger.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpriteChangerHH : MonoBehaviour
+public class PlayerSpriteChanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Sprite sprite1;
