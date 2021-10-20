37b2186024ada31e6eb233caaf7dddc9f2f6cb46
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-10-20 16:58:07 +0200

    Created ZooHH

---
+++ projects/ZooHH/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZooHH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
