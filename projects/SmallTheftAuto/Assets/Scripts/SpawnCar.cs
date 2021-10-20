c2c61ab0ceecad19073a1806085f712ece329b2a
InaSLew <ina.shenlewander@gmail.com>
2021-10-20 15:22:25 +0200

    Fixed exceptions thrown by CarPrefab

--- projects/SmallTheftAuto/Assets/Scripts/SpawnCar.cs
+++ projects/SmallTheftAuto/Assets/Scripts/SpawnCar.cs
@@ -5,13 +5,14 @@ public class SpawnCar : MonoBehaviour
 {
     public GameObject carPrefab;
 
-    private void Awake()
+    void Start()
     {
         var car = Instantiate(carPrefab);
         car.transform.position = new Vector3(0, 1.8f, 0);
         car.GetComponent<CarMovement>().enabled = false;
         
         var car2 = Instantiate(carPrefab);
+        Debug.Log("CAR2: " + car2);
         car2.transform.position = new Vector3(-10, 1.8f, 0);
         car2.GetComponent<CarMovement>().enabled = false;
     }
