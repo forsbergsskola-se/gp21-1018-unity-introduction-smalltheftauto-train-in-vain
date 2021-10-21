28b9bbd3db5d942b551358acfefffde685b3dc3b
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-10-21 09:30:17 +0200

    duplicate

--- projects/ZooHH/Program.cs
+++ projects/ZooHH/Program.cs
@@ -4,9 +4,30 @@ namespace ZooHH
 {
     class Program
     {
+        class Animal{}
+
+        class Student {}
+
+        class Mammal : Animal{}
+        class Bear : Mammal{}
+        class Donkey : Mammal{}
+        class Lion : Mammal{}
+        
+        class Fish : Animal{}
+        class Salmon : Fish{}
+        class Clownfish : Fish{}
+
+        public class Zoo<T> {}
+
+
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Zoo<fishZoo> = new Zoo
+
         }
     }
 }
