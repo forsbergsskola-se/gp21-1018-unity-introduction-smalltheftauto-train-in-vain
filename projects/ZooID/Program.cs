5f66a52bd3713898e1c6124856a54a958c2d3297
MustoLini <danielssonjohannes@gmail.com>
2021-10-21 10:32:13 +0200

    Update Program.cs

--- projects/ZooID/Program.cs
+++ projects/ZooID/Program.cs
@@ -11,6 +11,40 @@ namespace ZooID
                 fishZoo.AddAnimal(new Fish()); // OKAY
                 fishZoo.AddAnimal(new Clownfish()); // OKAY
             }
+            {
+                Zoo<Animal> animalZoo = new Zoo<Animal>();
+                animalZoo.AddAnimal(new Fish()); // OKAY
+                animalZoo.AddAnimal(new Clownfish()); // OKAY
+                animalZoo.AddAnimal(new Lion()); // OKAY
+                animalZoo.AddAnimal(new Donkey()); // OKAY
+                
+                Zoo<Lion> lionZoo = new Zoo<Lion>();
+                animalZoo.AddAnimal(new Lion()); // OKAY
+                animalZoo.AddAnimal(new Lion()); // OKAY
+                animalZoo.AddAnimal(new Lion()); // OKAY
+            }
+            {
+                Zoo<Salmon> salmonZoo = new Zoo<Salmon>();
+                salmonZoo.HasAnimal<Lion>(); // ERROR!
+            }
+            {
+                Zoo<Student> studentZoo = new StudentZoo(); // ERROR!
+            }
+            {
+                Zoo<Fish> fishZoo = new Zoo<Fish>();
+                fishZoo.AddAnimal(new Lion()); // ERROR!
+            }
+            {
+                Zoo<Salmon> salmonZoo = new Zoo<Salmon>();
+                salmonZoo.AddAnimal(new Fish()); // ERROR!
+            }
+            {
+                Zoo<Fish> fishZoo = new Zoo<Fish>();
+                fishZoo.AddAnimal(new Salmon());
+                fishZoo.AddAnimal(new Salmon());
+                Console.WriteLine("This should be False: "+fishZoo.HasAnimal<Clownfish>());
+            }
+            
         }
     }
     class Animal 
@@ -58,14 +92,19 @@ namespace ZooID
         
     }
 
-    class Zoo <T>  where T : Animal {
-        public Animal[] animals;
-        private int arraysize = 0;
+    class Zoo <TAnimals>  where TAnimals : Animal
+    {
+        private TAnimals[] Animals = new TAnimals[0];
 
-        public void AddAnimal(T Animal) 
+        public void AddAnimal(TAnimals animal) 
         { 
-            Array.Resize(ref animals, ++arraysize); 
-                
+            Array.Resize(ref Animals, Animals.Length+1);
+            Animals[Animals.Length - 1] = animal;
+        }
+
+        public void HasAnimal<TSpecies>() where TSpecies : TAnimals
+        {
+            throw new NotImplementedException();
         }
         
     }
