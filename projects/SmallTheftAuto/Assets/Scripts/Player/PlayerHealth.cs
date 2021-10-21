61bcb75e9a95eb6451ff0619ea2d6119651bdd4b
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-10-21 16:03:28 +0200

    Wasted Implemented

--- projects/SmallTheftAuto/Assets/Scripts/Player/PlayerHealth.cs
+++ projects/SmallTheftAuto/Assets/Scripts/Player/PlayerHealth.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    
     public int maxHealth = 100;
-
     public int currentHealth;
 
     private HealthBar HealthBar;
@@ -15,11 +13,11 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
         HealthBar = GameObject.FindWithTag("HealthBar").GetComponent<HealthBar>();
         currentHealth = maxHealth;
         HealthBar.sethealth(maxHealth);
         Wasted = GameObject.FindWithTag("Wasted");
+        Wasted.SetActive(false);
     }
 
     // Update is called once per frame
@@ -32,14 +30,12 @@ public class PlayerHealth : MonoBehaviour
     }
     void TakeDamage(int damage)
     {
-        
         currentHealth -= damage;
         HealthBar.sethealth(currentHealth);
         if (currentHealth<=0)
         {
             Wasted.SetActive(true);
-            Debug.Log("Player is dead.");
+            Destroy(this.gameObject);
         }
     }
-
 }
