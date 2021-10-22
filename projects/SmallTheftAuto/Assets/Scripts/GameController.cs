308c4fa27396ecac91a15bec01c2ecfad64de943
MustoLini <danielssonjohannes@gmail.com>
2021-10-22 10:58:33 +0200

    PhoneBox

--- projects/SmallTheftAuto/Assets/Scripts/GameController.cs
+++ projects/SmallTheftAuto/Assets/Scripts/GameController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject player;
+    public GameObject phoneBox;
     void Start()
     {
         Instantiate(player);
+        Instantiate(phoneBox).transform.position=new Vector3(-0.519f, 41.2f,0f);
     }
 
     void Update()
