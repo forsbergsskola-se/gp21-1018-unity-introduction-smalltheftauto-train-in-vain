d194a1ea3a22525f9fc0d0644ece12e46a4a5a65
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-10-26 10:51:13 +0200

    Fixed a naming error

--- projects/SmallTheftAuto/Assets/Scripts/Quest/Q1CarRace/FinishCarRace.cs
+++ projects/SmallTheftAuto/Assets/Scripts/Quest/Q1CarRace/FinishCarRace.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 
 public class FinishCarRace : MonoBehaviour
 {
-    private GameObject questCarCollider;
+    private GameObject questCar;
     
     private void Start()
     {
-        questCarCollider = FindObjectOfType<CarRaceController>().QuestCar.transform.Find("CarChassis").gameObject;
+        questCar = FindObjectOfType<CarRaceController>().QuestCar.transform.Find("CarChassis").gameObject;
     }
 
 
     private void OnTriggerEnter2D(Collider2D objectThatTriggered)
     {
-        if (objectThatTriggered.gameObject == questCarCollider)
+        if (objectThatTriggered.gameObject == questCar)
         {
             // Tell the CarRaceController that the race has been completed.
             gameObject.transform.parent.parent.gameObject.GetComponent<CarRaceController>().RaceCompleted(true,69);
