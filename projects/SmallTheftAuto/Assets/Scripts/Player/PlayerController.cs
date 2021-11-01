72276ca3d46c3d77f9504727141c9a79f204cac0
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-11-01 15:18:36 +0100

    Implemented a HUD script in canvas for easier access to child objects like text.

--- projects/SmallTheftAuto/Assets/Scripts/Player/PlayerController.cs
+++ projects/SmallTheftAuto/Assets/Scripts/Player/PlayerController.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    private TMP_Text scoreText;
+    
+    private int score;
+
+    public int Score
+    {
+        get => score;
+        set
+        {
+            score = value;
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         var followCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<FollowCamera>();
         followCamera.target = gameObject;
-        
+
+        scoreText = FindObjectOfType<HUD>().ScoreText;
+
+        Score = 50;
     }
 
     // Update is called once per frame
