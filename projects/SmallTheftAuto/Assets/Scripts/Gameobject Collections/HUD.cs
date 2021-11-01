d76d2a532d99180e04bf5866dc10ba5568e1aff0
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-11-01 16:04:37 +0100

    Added HUD pointers to weaponPanel

--- projects/SmallTheftAuto/Assets/Scripts/Gameobject Collections/HUD.cs
+++ projects/SmallTheftAuto/Assets/Scripts/Gameobject Collections/HUD.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HUD : MonoBehaviour
 {
     public TMP_Text ScoreText;
     public TMP_Text MoneyText;
     public GameObject QuestTimer;
+    public UnityEngine.UI.Image WeaponImage;
 }
