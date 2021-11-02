0fd966ebc5680dbd02f5b95fd76cc34125f7c44d
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-11-02 17:37:08 +0100

    Health power-up only works when not at full health

--- projects/SmallTheftAuto/Assets/HealthPowerUp.cs
+++ projects/SmallTheftAuto/Assets/HealthPowerUp.cs
@@ -7,21 +7,29 @@ public class HealthPowerUp : MonoBehaviour
 {
     private PlayerHealth playerHealth;
     public int healthUp;
+    private int currentHealth;
     
 
     void Start()
     {
         playerHealth = FindObjectOfType<PlayerHealth>();
+        currentHealth = playerHealth.currentHealth;
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log(other.name);
         
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && (currentHealth != 100))
         {
             playerHealth.TakeDamage(- healthUp);
             Destroy(gameObject);
+        }
+
+        else
+        {
+             Debug.Log("Player has full health");
         }
     }
 }
