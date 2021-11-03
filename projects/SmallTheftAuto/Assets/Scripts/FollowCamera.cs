932f77a5e75ffcb3c90fd77b690db5bfc0d4b185
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-11-03 17:39:16 +0100

    Update FollowCamera.cs

--- projects/SmallTheftAuto/Assets/Scripts/FollowCamera.cs
+++ projects/SmallTheftAuto/Assets/Scripts/FollowCamera.cs
@@ -9,11 +9,14 @@ public class FollowCamera : MonoBehaviour
     
     void Update()
     {
-        var cameraCoordinates = transform.position;
-        var targetCoordinates = target.transform.position;
-        var newCameraCoordinates = Vector3.Lerp(cameraCoordinates, targetCoordinates, 0.1f);
-        // newCameraCoordinates.; = CameraHeightOffset;
+        if (target != null)
+        {
+            var cameraCoordinates = transform.position;
+            var targetCoordinates = target.transform.position;
+            var newCameraCoordinates = Vector3.Lerp(cameraCoordinates, targetCoordinates, 0.1f);
+            // newCameraCoordinates.; = CameraHeightOffset;
 
-        transform.position = new Vector3(newCameraCoordinates.x, newCameraCoordinates.y, CameraHeightOffset);
+            transform.position = new Vector3(newCameraCoordinates.x, newCameraCoordinates.y, CameraHeightOffset);
+        }
     }
 }
