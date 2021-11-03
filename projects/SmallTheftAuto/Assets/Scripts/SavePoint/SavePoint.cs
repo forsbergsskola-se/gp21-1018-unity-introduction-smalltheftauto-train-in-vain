f3c0d50d1a9db41ef2e167a1f989e09344e7f99c
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-11-03 16:16:14 +0100

    Save point work

--- projects/SmallTheftAuto/Assets/Scripts/SavePoint/SavePoint.cs
+++ projects/SmallTheftAuto/Assets/Scripts/SavePoint/SavePoint.cs
@@ -2,14 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class SavePoint : MonoBehaviour, IInteractable
 {
     private static bool TurnYellow;
+
+    public static int NextId;
+    public int Id;
+
+    private PlayerController player;
+    private PlayerHealth playerHealth;
     
     private void Start()
     {
         FindObjectOfType<PlayerInteract>().Interactables.Add(gameObject);
+        Id = NextId;
+        NextId++;
+        player = FindObjectOfType<PlayerController>();
+        playerHealth = FindObjectOfType<PlayerHealth>();
     }
 
     private void Update()
@@ -35,6 +46,11 @@ public class SavePoint : MonoBehaviour, IInteractable
 
     void Save()
     {
-        // TODO: ADD SAVE CODE HERE! -----------------------------------------------------------------------------------
+        StreamWriter save = new StreamWriter("Save.txt");
+        save.WriteLine(Id);
+        save.WriteLine(playerHealth.currentHealth);
+        save.WriteLine(player.Score);
+        save.WriteLine(player.Money);
+        save.Close();
     }
 }
