c3d2ab69cf37d1e8ada4ed061249d7b913ce0b0a
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2021-11-03 16:20:11 +0100

    PushID Save and Load

--- projects/SmallTheftAuto/Assets/Scripts/SavePoint/LoadSavePointID.cs
+++ projects/SmallTheftAuto/Assets/Scripts/SavePoint/LoadSavePointID.cs
@@ -1,11 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using System.IO;
+using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class LoadSavePointID : MonoBehaviour
 {
-    public void LoadSave()
+    private static bool LoadOnStart;
+
+    public static void LoadSave()
     {
-        Debug.Log("Loading save...");
+        Debug.Log("Loading save... OA");
+        LoadOnStart = true;
+        SavePointID.NextID = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void Start()
+    {
+        if(LoadOnStart)
+        {
+            Invoke("PlacePlayer", 1);
+        }
+    }
+
+    void PlacePlayer()
+    {
+        // Debug.Log("Hello there!");
+        StreamReader loader = new StreamReader("Save.txt");
+        var playerHealth = FindObjectOfType<PlayerHealth>();
+        var playerController = FindObjectOfType<PlayerController>();
+        var player = GameObject.FindGameObjectWithTag("Player");
+        // TODO FIX POSITION
+        var savePointIdToFind = Int32.Parse(loader.ReadLine());
+        Debug.Log(savePointIdToFind);
+        var savePointsID = FindObjectsOfType<SavePointID>().ToList();
+        var found = savePointsID.Find(x => x.Id == savePointIdToFind);
+        found.GetComponent<Transform>();
+        player.transform.position = found.GetComponent<Transform>().position;
+
+
+
+
+        playerHealth.TakeDamage(playerHealth.maxHealth - Int32.Parse(loader.ReadLine()));
+        playerController.Score = Int32.Parse(loader.ReadLine());
+        playerController.addMoney(Int32.Parse(loader.ReadLine()));
+        loader.Close();
     }
 }
--- projects/SmallTheftAuto/Assets/Scripts/SavePoint/SavePointID.cs
+++ projects/SmallTheftAuto/Assets/Scripts/SavePoint/SavePointID.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
-public class SavePointID : MonoBehaviour
+public class SavePointID : MonoBehaviour, IInteractable
 {
     private static bool TurnYellow;
-    
+
+    public static int NextID;
+    public int Id;
+
+    private PlayerHealth playerHealth;
+    private PlayerController player;
     private void Start()
     {
         FindObjectOfType<PlayerInteract>().Interactables.Add(gameObject);
+        Id = NextID;
+        NextID++;
+        player = FindObjectOfType<PlayerController>();
+        playerHealth = FindObjectOfType<PlayerHealth>();
     }
 
     private void Update()
@@ -35,5 +46,11 @@ public class SavePointID : MonoBehaviour
     void Save()
     {
         // TODO: ADD SAVE CODE HERE! -----------------------------------------------------------------------------------
+        StreamWriter save = new StreamWriter("Save.txt");
+        save.WriteLine(Id);
+        save.WriteLine(playerHealth.currentHealth);
+        save.WriteLine(player.Score);
+        save.WriteLine(player.Money);
+        save.Close();
     }
 }
