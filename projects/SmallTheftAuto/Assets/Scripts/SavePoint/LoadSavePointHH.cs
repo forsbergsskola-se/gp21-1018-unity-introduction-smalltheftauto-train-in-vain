d87ffbb70687a267e8e73990db697657b264b67f
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-11-03 14:04:13 +0100

    Prepared for shared work on savepoint

---
+++ projects/SmallTheftAuto/Assets/Scripts/SavePoint/LoadSavePointHH.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSavePointHH : MonoBehaviour
+{
+    public void LoadSave()
+    {
+        Debug.Log("Loading save...");
+    }
+}
---
+++ projects/SmallTheftAuto/Assets/Scripts/SavePoint/SavePointISL.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePointISL : MonoBehaviour
+{
+    private static bool TurnYellow;
+    
+    private void Start()
+    {
+        FindObjectOfType<PlayerInteract>().Interactables.Add(gameObject);
+    }
+
+    private void Update()
+    {
+        if (TurnYellow)
+        {
+            GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
+    }
+
+    public void Interact(GameObject User)
+    {
+        TurnYellow = true;
+        Invoke("MakeGreen", 0.25f);
+    }
+
+    void MakeGreen()
+    {
+        TurnYellow = false;
+        GetComponent<SpriteRenderer>().color = Color.green;
+        Save();
+    }
+
+    void Save()
+    {
+        // TODO: ADD SAVE CODE HERE! -----------------------------------------------------------------------------------
+    }
+}
