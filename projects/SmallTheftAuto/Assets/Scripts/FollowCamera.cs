4c1d2f41d4394de5ae4727b6a0c3c2794b27a221
InaSLew <ina.shenlewander@gmail.com>
2021-11-05 21:14:22 +0100

    try moving camera follow to FixedUpdate

--- projects/SmallTheftAuto/Assets/Scripts/FollowCamera.cs
+++ projects/SmallTheftAuto/Assets/Scripts/FollowCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,19 @@ public class FollowCamera : MonoBehaviour
     public float CameraHeightOffset = -40;
     public GameObject target;
     
-    void Update()
+    // void Update()
+    // {
+    //     if (target != null)
+    //     {
+    //         var cameraCoordinates = transform.position;
+    //         var targetCoordinates = target.transform.position;
+    //         var newCameraCoordinates = Vector3.Lerp(cameraCoordinates, targetCoordinates, 0.1f);
+    //         // newCameraCoordinates.; = CameraHeightOffset;
+    //
+    //         transform.position = new Vector3(newCameraCoordinates.x, newCameraCoordinates.y, CameraHeightOffset);
+    //     }
+    // }
+    private void FixedUpdate()
     {
         if (target != null)
         {
