918a30bdf29a80ce79769e87875f6094bbadb8fb
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-11-05 13:26:56 +0100

    Updated NPCSpawner to support other positions.

--- projects/SmallTheftAuto/Assets/Scripts/NPCSpawner.cs
+++ projects/SmallTheftAuto/Assets/Scripts/NPCSpawner.cs
@@ -40,22 +40,27 @@ public class NPCSpawner : MonoBehaviour
         NPCs.RemoveAll(x => x == null);
         if (NPCs.Count < MaxNPCs)
         {
-            var newNPC = Instantiate(NPCPrefab);
-            var temp = Random.Range(0, spawnPositions.Count);
-            newNPC.transform.position = spawnPositions[temp];
-            newNPC.GetComponent<SpriteRenderer>().sprite = NPCskins[Random.Range(0, NPCskins.Length)];
-
-            var newNPCVariables = newNPC.GetComponent<Pedestrian>();
-
-            newNPCVariables.MaxHealth = Random.Range(MaxHealthRange[0], MaxHealthRange[1]);
-            newNPCVariables.MoveSpeed = Random.Range(MoveSpeedRange[0], MoveSpeedRange[1]);
-            newNPCVariables.WaitTimeMax = Random.Range(WaitTimeMaxRange[0], WaitTimeMaxRange[1]);
-            newNPCVariables.WaitTimeMin = Random.Range(WaitTimeMinRange[0], WaitTimeMinRange[1]);
-            newNPCVariables.PanicModeTime = Random.Range(PanicModeTimeRange[0], PanicModeTimeRange[1]);
-
-            Debug.Log("Spawned a new NPC at spawnID: " + temp);
-            NPCs.Add(newNPC);
-            Debug.Log($"NPC has nr: {NPCs.Count}");
+            var spawnPosition = spawnPositions[Random.Range(0, spawnPositions.Count)];
+            SpawnNewNPC(spawnPosition);
         }
+    }
+
+    void SpawnNewNPC(Vector3 position)
+    {
+        var newNPC = Instantiate(NPCPrefab);
+        newNPC.transform.position = position;
+        newNPC.GetComponent<SpriteRenderer>().sprite = NPCskins[Random.Range(0, NPCskins.Length)];
+
+        var newNPCVariables = newNPC.GetComponent<Pedestrian>();
+
+        newNPCVariables.MaxHealth = Random.Range(MaxHealthRange[0], MaxHealthRange[1]);
+        newNPCVariables.MoveSpeed = Random.Range(MoveSpeedRange[0], MoveSpeedRange[1]);
+        newNPCVariables.WaitTimeMax = Random.Range(WaitTimeMaxRange[0], WaitTimeMaxRange[1]);
+        newNPCVariables.WaitTimeMin = Random.Range(WaitTimeMinRange[0], WaitTimeMinRange[1]);
+        newNPCVariables.PanicModeTime = Random.Range(PanicModeTimeRange[0], PanicModeTimeRange[1]);
+        
+        
+        NPCs.Add(newNPC);
+        Debug.Log($"NPC has nr: {NPCs.Count}");
     }
 }
