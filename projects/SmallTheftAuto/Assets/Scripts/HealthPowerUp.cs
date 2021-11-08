0985985605e6444bce5ff7acdc4712dead27a77e
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-11-09 00:11:36 +0100

    added an update method to fix null reference Player error

--- projects/SmallTheftAuto/Assets/Scripts/HealthPowerUp.cs
+++ projects/SmallTheftAuto/Assets/Scripts/HealthPowerUp.cs
@@ -9,17 +9,19 @@ public class HealthPowerUp : MonoBehaviour
     private PlayerController playerController;
     
 
-    void Start()
+    void LateUpdate()
     {
         playerController = FindObjectOfType<PlayerController>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log(other.name);
+
         if (other.CompareTag("Player") && (playerController.Health != playerController.MaxHealth))
         {
             playerController.TakeDamage(-healthUp);
-            Destroy(gameObject);
+            Destroy(this.gameObject);
         }
     }
 }
